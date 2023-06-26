5167f6cfcece3bb3401334dda52e4aa26507d072
CuongTran <sonnv@30shine.com>
2023-06-26 18:56:02 +0700

    test

---
+++ pages/IndexPage.ts
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import Tutorial from "@components/Tutorial.vue"; // Đường dẫn tới file Tutorial.vue
+
+
+export default Vue.extend({
+  name: 'IndexPage',
+//   components: {
+//     Tutorial // Đăng ký component trong phạm vi của IndexPage
+//   }
+});
--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,16 @@
   <Tutorial/>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
+<script lang="ts" src="./IndexPage.ts"></script>
+
+<!-- <script lang="ts">
+import Vue from 'vue';
+import Tutorial from '@/components/Tutorial.vue'; // Đường dẫn tới file Tutorial.vue
 
 export default Vue.extend({
-  name: 'IndexPage'
+  name: 'IndexPage',
+  components: {
+    Tutorial // Đăng ký component trong phạm vi của IndexPage
+  }
 })
-</script>
+</script> -->
